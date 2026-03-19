fe00b3ec46f148f626992f9f720bd21c64490123
Isaac N Njenga <isaac.njenga@mediacent.africa>
2026-03-19 16:42:58 +0300

    is that 4 me

--- front/src/contexts/NotificationContext/index.js
+++ front/src/contexts/NotificationContext/index.js
@@ -1,18 +1,22 @@
 import React, { createContext, useContext } from "react";
 import { notification } from "antd";
+import { useAuth } from "../AuthContext";
 
 const NotificationContext = createContext(null);
 
 export const NotificationProvider = ({ children }) => {
+  const { isAuthenticated } = useAuth();
   const [api, contextHolder] = notification.useNotification();
 
   const openNotification = (type, message, title) => {
-    api[type]({
-      title: <span style={{ fontFamily: "DM Sans" }}>{title}</span>,
-      description: <span style={{ fontFamily: "DM Sans" }}>{message}</span>,
-      placement: "topRight",
-      duration: 3,
-    });
+    if (isAuthenticated) {
+      api[type]({
+        title: <span style={{ fontFamily: "DM Sans" }}>{title}</span>,
+        description: <span style={{ fontFamily: "DM Sans" }}>{message}</span>,
+        placement: "topRight",
+        duration: 2,
+      });
+    }
   };
 
   return (

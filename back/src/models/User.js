4d9d1c931e90a5661be80999dc085fcdc6e8c437
Isaac N Njenga <isaac.njenga@mediacent.africa>
2026-03-26 21:02:25 +0300

    sfsf

--- back/src/models/User.js
+++ back/src/models/User.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -7,32 +6,11 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, minLength: 8, select: false },
     avatar: { type: String, default: "" },
+    passwordReset: { type: Boolean, default: false },
     refreshToken: { type: String, default: "" },
   },
   { collection: "users", timestamps: true },
 );
-
-//hashing password before saving user
-userSchema.pre("save", async function () {
-  if (!this.isModified("password")) {
-    return;
-  }
-  const salt = await bcrypt.genSalt(10);
-  this.password =  bcrypt.hash(this.password, salt);
-});
-
-//compare password
-userSchema.methods.comparePassword = async function (userPassword) {
-  // console.log("Comparing passwords:", {
-  //   userPassword,
-  //   storedHash: this.password,
-  // });
-  // console.log(
-  //   "bcrypt.compare result:",
-  //   await bcrypt.compare(userPassword, this.password),
-  // );
-  return await bcrypt.compare(userPassword, this.password);
-};
 
 const UserModel = mongoose.model("User", userSchema);
 

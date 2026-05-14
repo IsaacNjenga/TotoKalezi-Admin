9490532a80c5bf9abfce6684d7873ac19dd3803e
Isaac Njenga <isaac.njenga@mediacent.africa>
2026-05-14 15:23:28 +0300

    dreams

--- back/src/models/User.js
+++ back/src/models/User.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcryptjs";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
@@ -11,6 +12,17 @@ const userSchema = new mongoose.Schema(
   },
   { collection: "users", timestamps: true },
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
 const UserModel = mongoose.model("User", userSchema);
 
